c5f0522bbceeb828e3971b7117b5564b7e5f22cc
PhanLeThanhPhuc <pltp2010@gmail.com>
2023-10-01 11:00:04 +0700

    test

---
+++ src/main/java/thanhphuc/asmjava5/TestThanhphucAzureApplication.java
@@ -0,0 +1,13 @@
+package thanhphuc.asmjava5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestThanhphucAzureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestThanhphucAzureApplication.class, args);
+	}
+
+}
---
+++ src/test/java/thanhphuc/asmjava5/TestThanhphucAzureApplicationTests.java
@@ -0,0 +1,13 @@
+package thanhphuc.asmjava5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestThanhphucAzureApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
